a15a68aac538dee1057283d77ff0a5fca8780afe
MaddieGillis <112782694+MaddieGillis@users.noreply.github.com>
2023-03-09 12:41:13 -0800

    started User model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,30 @@
 const { Schema, model } = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String, 
+            required: true, 
+            unique: true, 
+            trim: true ,
+
+        },
+        
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i]
+        },
+
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }],
+
+       friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+       }] 
+    }
+)
