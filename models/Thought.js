b70d1ffb581bca69c56b2c5cd7498f27cb28c23b
MaddieGillis <112782694+MaddieGillis@users.noreply.github.com>
2023-03-11 14:22:13 -0800

    Completed Tought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,37 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+
+const reactionSchema = new mongoose.Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId();
+        },
+
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+
+        username: {
+            type: String,
+            required: true
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }
+);
+
+//Sets timestamps for reactionSchema
+reactionSchema.set('toObject', { getters: true }); 
+reactionSchema.set('toJSON', { getters: true }); 
+
+reactionSchema.path('createdAt').get(function(value) {
+  return value.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' });
+});
 
 const thoughtSchema = new mongoose.Schema(
     {
@@ -17,17 +50,25 @@ const thoughtSchema = new mongoose.Schema(
         username: {
             type: String,
             required: true
-        }
+        }.
+
+        reactions: [reactionScheama]
 
     }
 );
 
+
+//Sets timesstamps for thoughtSchema
 thoughtSchema.set('toObject', { getters: true });
 thoughtSchema.set('toJSON', { getters: true });
 
 thoughtSchema.path('createdAt').getfunction(value) {
     return value.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' });
 }
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+})
 
 
 const Thought = model('Thought', thoughtSchema);
